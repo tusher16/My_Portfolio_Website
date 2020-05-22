b4d09fb26f710ce177905eeb02f318ff06c5c697
tusher16 <tusher16@gmail.com>
2020-05-22 16:46:30 +0600

    django static page done

--- home/models.py
+++ home/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=50)
+    recent_status = models.CharField(max_length=100)
+
+    facebook_link = models.URLField(("facebook_link"), 
+        max_length=128, 
+        db_index=True, 
+        unique=True, 
+        blank=True
+    )
+    linkedin_link = models.URLField(("linkedin_link"), 
+        max_length=128, 
+        db_index=True, 
+        unique=True, 
+        blank=True
+    )
+    github_link = models.URLField(("github_link"), 
+        max_length=128, 
+        db_index=True, 
+        unique=True, 
+        blank=True
+    )
+    stack_overflow_link = models.URLField(("stack_overflow_link"), 
+        max_length=128, 
+        db_index=True, 
+        unique=True, 
+        blank=True
+    )
