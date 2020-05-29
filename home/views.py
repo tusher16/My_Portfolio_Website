661d55f66bcadb7227f04b51554a1f218e41c1ca
tusher16 <tusher16@gmail.com>
2020-05-29 15:25:41 +0600

    static files updated, models and views added

--- home/views.py
+++ home/views.py
@@ -2,12 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .models import *
+
 def home(request):
-    context = {}
+
+    profile = Profile.objects.all()
+
+    
+    
+    context = {'Profile': Profile}
     return render(request, 'home/index.html', context)
 
 
 def header(request):
-    context = {}
+    profile = Profile.objects.all()
+
+    
+    
+    context = {'Profile': Profile}
+    
     return render(request, 'home/header.html', context)
 
--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.conf.urls.static import static 
 from django.conf import settings
@@ -24,4 +25,5 @@ urlpatterns = [
     path ('', include('home.urls'))
 ]
 
+urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
