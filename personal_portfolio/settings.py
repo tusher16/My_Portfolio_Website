1e3e96ae0143e1ef1d61f61943b65bda12a4e53b
Mohammad obaidullah Tusher <tusher16@gmail.com>
2020-12-17 00:31:22 +0600

    Update settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q)y&&=-*a&%++3#i8v@h^7db=5psmygjhuh25g=8(h@su16_df'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
